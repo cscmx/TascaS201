5c4eee48b1f76337c92f13d4fb5999d16b241684
CSC <82180341+cscmx@users.noreply.github.com>
2026-07-19 23:19:12 +0200

    Add files via upload
    Refactor: update sql cuello de botella to requested modifications

--- optica_cuelloDeBotella.sql
+++ optica_cuelloDeBotella.sql
@@ -1,40 +1,151 @@
-USE `optica_cuelloDeBotella`;
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema optica_cuelloDeBotella
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema optica_cuelloDeBotella
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `optica_cuelloDeBotella` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `optica_cuelloDeBotella` ;
+
+-- -----------------------------------------------------
+-- Table `optica_cuelloDeBotella`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica_cuelloDeBotella`.`cliente` (
+  `id_cliente` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `direccion` VARCHAR(45) NOT NULL,
+  `telefono` VARCHAR(15) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `fecha_registro` DATE NOT NULL,
+  `cliente_id_cliente` INT NULL,
+  PRIMARY KEY (`id_cliente`),
+  INDEX `fk_cliente_cliente1_idx` (`cliente_id_cliente` ASC) VISIBLE,
+  CONSTRAINT `fk_cliente_cliente1`
+    FOREIGN KEY (`cliente_id_cliente`)
+    REFERENCES `optica_cuelloDeBotella`.`cliente` (`id_cliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb3;
 
 
+-- -----------------------------------------------------
+-- Table `optica_cuelloDeBotella`.`empleado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica_cuelloDeBotella`.`empleado` (
+  `id_empleado` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_empleado`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb3;
 
--- proveedor
-INSERT INTO proveedor (nombre, direccion, telefono, fax, nif) VALUES
-('Universium', 'Gran Via 345', '932456321', '932456322', 'B12345678'),
-('Galaxy', 'Paseo Colón 25', '931231231', '931231232', 'B87654321');
 
--- marca
-INSERT INTO marca (`nombre_marca`, proveedor_id_proveedor) VALUES
-('Ray Ban', 1),
-('Oakley', 1),
-('Etnia', 2);
+-- -----------------------------------------------------
+-- Table `optica_cuelloDeBotella`.`proveedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica_cuelloDeBotella`.`proveedor` (
+  `id_proveedor` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `direccion` VARCHAR(45) NOT NULL,
+  `telefono` VARCHAR(15) NOT NULL,
+  `fax` VARCHAR(15) NOT NULL,
+  `nif` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`id_proveedor`),
+  UNIQUE INDEX `nif_UNIQUE` (`nif` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb3;
 
--- clientes
-INSERT INTO cliente (nombre, direccion, telefono, email, `fecha_registro`, cliente_id_cliente) VALUES
-('Pedro Guerrero', 'Plaza Prim 2', '612312312', 'pedro@gmail.com', '2025-06-10', NULL),
-('Joana Monet', 'Gran Vía 400', '656756756', 'joana@gmail.com', '2025-07-15', 1);
 
--- empleados
-INSERT INTO empleado (nombre) VALUES
-('Marina López'),
-('Óscar Rodríguez');
+-- -----------------------------------------------------
+-- Table `optica_cuelloDeBotella`.`marca`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica_cuelloDeBotella`.`marca` (
+  `id_marca` INT NOT NULL AUTO_INCREMENT,
+  `nombre_marca` VARCHAR(45) NOT NULL,
+  `proveedor_id_proveedor` INT NOT NULL,
+  PRIMARY KEY (`id_marca`),
+  INDEX `fk_marca_proveedor1_idx` (`proveedor_id_proveedor` ASC) VISIBLE,
+  CONSTRAINT `fk_marca_proveedor1`
+    FOREIGN KEY (`proveedor_id_proveedor`)
+    REFERENCES `optica_cuelloDeBotella`.`proveedor` (`id_proveedor`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
+DEFAULT CHARACTER SET = utf8mb3;
 
--- pedidos
-INSERT INTO pedido (fecha, `precio_total`, cliente_id_cliente, empleado_id_empleado) VALUES
-('2026-01-10', 157.40, 1, 1),
-('2026-03-05', 99.85, 2, 2),
-('2026-05-20', 487.90, 1, 1);
 
--- gafas
-INSERT INTO gafas (`grad_derecho`, `grad_izquierdo`, `tipo_montura`, `color_vidrio`, precio, `color_montura`, marca_id_marca, pedido_id_pedido) VALUES
-(4, 4, 'pasta', 'Transparente', 250.00, 'Carey', 2, 1),
-(1, 1, 'metalica', 'Marrón', 90.00, 'Dorado', 3, 2),
-(3, 3, 'pasta', 'Verde', 140.00, 'Negro', 2, 3),
-(2, 2, 'flotante', 'Transparente', 300.00, 'Plata', 1, 3),
-(1, 1, 'metalica', 'Verde', 80.00, 'Plateado', 2, NULL); -- en stock
+-- -----------------------------------------------------
+-- Table `optica_cuelloDeBotella`.`pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica_cuelloDeBotella`.`pedido` (
+  `id_pedido` INT NOT NULL AUTO_INCREMENT,
+  `fecha` DATETIME NOT NULL,
+  `precio_total` DECIMAL(10,2) NOT NULL,
+  `empleado_id_empleado` INT NOT NULL,
+  `cliente_id_cliente` INT NOT NULL,
+  PRIMARY KEY (`id_pedido`),
+  INDEX `fk_pedido_empleado1_idx` (`empleado_id_empleado` ASC) VISIBLE,
+  INDEX `fk_pedido_cliente1_idx` (`cliente_id_cliente` ASC) VISIBLE,
+  CONSTRAINT `fk_pedido_empleado1`
+    FOREIGN KEY (`empleado_id_empleado`)
+    REFERENCES `optica_cuelloDeBotella`.`empleado` (`id_empleado`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pedido_cliente1`
+    FOREIGN KEY (`cliente_id_cliente`)
+    REFERENCES `optica_cuelloDeBotella`.`cliente` (`id_cliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
+DEFAULT CHARACTER SET = utf8mb3;
 
-SELECT * FROM gafas;
+
+-- -----------------------------------------------------
+-- Table `optica_cuelloDeBotella`.`gafas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica_cuelloDeBotella`.`gafas` (
+  `id_gafas` INT NOT NULL AUTO_INCREMENT,
+  `grad_derecho` DECIMAL(10,2) NOT NULL,
+  `grad_izquierdo` DECIMAL(10,2) NOT NULL,
+  `tipo_montura` ENUM('flotante', 'pasta', 'metalica') NOT NULL COMMENT 'TIPOS DE MONTURA\\\\n\\\\nFlotante\\\\nPasta\\\\nMetálica\\\\n',
+  `color_vidrio` VARCHAR(15) NOT NULL,
+  `precio` DECIMAL(10,2) NOT NULL,
+  `color_montura` VARCHAR(15) NOT NULL,
+  `pedido_id_pedido` INT NULL,
+  `marca_id_marca` INT NOT NULL,
+  PRIMARY KEY (`id_gafas`),
+  INDEX `fk_gafas_pedido1_idx` (`pedido_id_pedido` ASC) VISIBLE,
+  INDEX `fk_gafas_marca1_idx` (`marca_id_marca` ASC) VISIBLE,
+  CONSTRAINT `fk_gafas_pedido1`
+    FOREIGN KEY (`pedido_id_pedido`)
+    REFERENCES `optica_cuelloDeBotella`.`pedido` (`id_pedido`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_gafas_marca1`
+    FOREIGN KEY (`marca_id_marca`)
+    REFERENCES `optica_cuelloDeBotella`.`marca` (`id_marca`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
