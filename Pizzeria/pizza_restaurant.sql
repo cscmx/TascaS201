144993178549559aa7ef6a98b6f76b0bc1d460a1
CSC <82180341+cscmx@users.noreply.github.com>
2026-07-20 19:44:12 +0200

    Add files via upload
    pizza_restaurant.sql modificado con worker id (null) para entregas delivery

--- Pizzeria/pizza_restaurant.sql
+++ Pizzeria/pizza_restaurant.sql
@@ -21,7 +21,7 @@ USE `pizza_restaurant` ;
 -- Table `pizza_restaurant`.`client`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`client` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_client` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL COMMENT 'Name of clients',
   `lastname` VARCHAR(45) NOT NULL COMMENT 'client’s last-name',
   `address` VARCHAR(45) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`client` (
   `city` VARCHAR(45) NOT NULL,
   `province` VARCHAR(45) NOT NULL,
   `phone_number` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id_client`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -39,41 +39,12 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `pizza_restaurant`.`restaurant`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`restaurant` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_restaurant` INT NOT NULL AUTO_INCREMENT,
   `address` VARCHAR(45) NOT NULL,
   `zip_code` VARCHAR(10) NOT NULL,
   `city` VARCHAR(45) NOT NULL,
   `province` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `pizza_restaurant`.`client_order`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`client_order` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `date` DATETIME NOT NULL COMMENT 'Date and time of the order',
-  `order_type` ENUM('table', 'delivery') NOT NULL COMMENT 'TYPE OF ORDER:\\n\\n0 = TO-COLLECT\\n\\n1 = TO-GO',
-  `total_price` DECIMAL(10,2) NOT NULL,
-  `restaurant_id` INT NOT NULL,
-  `delivery_time` DATETIME NULL,
-  `client_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_order_restaurant1_idx` (`restaurant_id` ASC) VISIBLE,
-  INDEX `fk_client_order_client1_idx` (`client_id` ASC) VISIBLE,
-  CONSTRAINT `fk_order_restaurant1`
-    FOREIGN KEY (`restaurant_id`)
-    REFERENCES `pizza_restaurant`.`restaurant` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_client_order_client1`
-    FOREIGN KEY (`client_id`)
-    REFERENCES `pizza_restaurant`.`client` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id_restaurant`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -83,26 +54,9 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `pizza_restaurant`.`position`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`position` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `position_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `pizza_restaurant`.`products`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`products` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `image` BLOB NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  `product_type` ENUM('pizza', 'burger', 'drink') NOT NULL,
-  `category_pizza` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
+  `id_position` INT NOT NULL AUTO_INCREMENT,
+  `position` ENUM('delivery', 'cook') NOT NULL,
+  PRIMARY KEY (`id_position`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -112,26 +66,79 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `pizza_restaurant`.`worker`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`worker` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_worker` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `lastname` VARCHAR(45) NOT NULL,
   `nif` VARCHAR(15) NOT NULL,
   `phone_number` VARCHAR(10) NOT NULL,
-  `restaurant_id` INT NOT NULL,
-  `position_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_worker_restaurant1_idx` (`restaurant_id` ASC) VISIBLE,
-  INDEX `fk_worker_position1_idx` (`position_id` ASC) VISIBLE,
-  CONSTRAINT `fk_worker_restaurant1`
-    FOREIGN KEY (`restaurant_id`)
-    REFERENCES `pizza_restaurant`.`restaurant` (`id`)
+  `position_id_position` INT NOT NULL,
+  `restaurant_id_restaurant` INT NOT NULL,
+  PRIMARY KEY (`id_worker`),
+  INDEX `fk_worker_position1_idx` (`position_id_position` ASC) VISIBLE,
+  INDEX `fk_worker_restaurant1_idx` (`restaurant_id_restaurant` ASC) VISIBLE,
+  CONSTRAINT `fk_worker_position1`
+    FOREIGN KEY (`position_id_position`)
+    REFERENCES `pizza_restaurant`.`position` (`id_position`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_worker_position1`
-    FOREIGN KEY (`position_id`)
-    REFERENCES `pizza_restaurant`.`position` (`id`)
+  CONSTRAINT `fk_worker_restaurant1`
+    FOREIGN KEY (`restaurant_id_restaurant`)
+    REFERENCES `pizza_restaurant`.`restaurant` (`id_restaurant`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `pizza_restaurant`.`clientOrder`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`clientOrder` (
+  `id_clientOrder` INT NOT NULL AUTO_INCREMENT,
+  `date` DATETIME NOT NULL COMMENT 'Date and time of the order',
+  `order_type` ENUM('table', 'delivery') NOT NULL COMMENT 'TYPE OF ORDER:\\n\\n0 = TO-COLLECT\\n\\n1 = TO-GO',
+  `total_price` DECIMAL(10,2) NOT NULL,
+  `delivery_time` DATETIME NULL,
+  `client_id_client` INT NOT NULL,
+  `restaurant_id_restaurant` INT NOT NULL,
+  `worker_id_worker` INT NULL,
+  PRIMARY KEY (`id_clientOrder`),
+  INDEX `fk_clientOrder_client1_idx` (`client_id_client` ASC) VISIBLE,
+  INDEX `fk_clientOrder_restaurant1_idx` (`restaurant_id_restaurant` ASC) VISIBLE,
+  INDEX `fk_clientOrder_worker1_idx` (`worker_id_worker` ASC) VISIBLE,
+  CONSTRAINT `fk_clientOrder_client1`
+    FOREIGN KEY (`client_id_client`)
+    REFERENCES `pizza_restaurant`.`client` (`id_client`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_clientOrder_restaurant1`
+    FOREIGN KEY (`restaurant_id_restaurant`)
+    REFERENCES `pizza_restaurant`.`restaurant` (`id_restaurant`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_clientOrder_worker1`
+    FOREIGN KEY (`worker_id_worker`)
+    REFERENCES `pizza_restaurant`.`worker` (`id_worker`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `pizza_restaurant`.`products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`products` (
+  `id_products` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `image` BLOB NOT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  `product_type` ENUM('pizza', 'burger', 'drink') NOT NULL,
+  `category_pizza` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_products`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -141,20 +148,20 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `pizza_restaurant`.`quantity`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`quantity` (
-  `client_order_id` INT NOT NULL,
-  `products_id` INT NOT NULL,
+  `clientOrder_id_clientOrder` INT NOT NULL,
+  `products_id_products` INT NOT NULL,
   `quantity` INT NOT NULL,
-  PRIMARY KEY (`client_order_id`, `products_id`),
-  INDEX `fk_client_order_has_products_products1_idx` (`products_id` ASC) VISIBLE,
-  INDEX `fk_client_order_has_products_client_order1_idx` (`client_order_id` ASC) VISIBLE,
-  CONSTRAINT `fk_client_order_has_products_client_order1`
-    FOREIGN KEY (`client_order_id`)
-    REFERENCES `pizza_restaurant`.`client_order` (`id`)
+  PRIMARY KEY (`clientOrder_id_clientOrder`, `products_id_products`),
+  INDEX `fk_clientOrder_has_products_products1_idx` (`products_id_products` ASC) VISIBLE,
+  INDEX `fk_clientOrder_has_products_clientOrder1_idx` (`clientOrder_id_clientOrder` ASC) VISIBLE,
+  CONSTRAINT `fk_clientOrder_has_products_clientOrder1`
+    FOREIGN KEY (`clientOrder_id_clientOrder`)
+    REFERENCES `pizza_restaurant`.`clientOrder` (`id_clientOrder`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_client_order_has_products_products1`
-    FOREIGN KEY (`products_id`)
-    REFERENCES `pizza_restaurant`.`products` (`id`)
+  CONSTRAINT `fk_clientOrder_has_products_products1`
+    FOREIGN KEY (`products_id_products`)
+    REFERENCES `pizza_restaurant`.`products` (`id_products`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
